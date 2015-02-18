1e53ecc87c2d6e006a825e8409b1f4683266b848
oranjuice <oranjuices@hotmail.com>
2015-02-18 16:59:27 +0530

    Unwanted printf

--- src/wap_server.c
+++ src/wap_server.c
@@ -184,7 +184,6 @@ static void
 start_mining_process (client_t *self)
 {
     IPC::Daemon::start_mining(self->message);
-    printf("\n\n Request: %d \n\n", (int)wap_proto_start_height(self->message));
 }
 
 
